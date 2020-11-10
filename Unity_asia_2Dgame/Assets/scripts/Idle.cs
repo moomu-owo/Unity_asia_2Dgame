54468a0ccc6d8cf193cd379404ef8d467dc6544f
moomu-owo <71679979+moomu-owo@users.noreply.github.com>
2020-11-10 16:13:41 +0800

    finished

--- Unity_asia_2Dgame/Assets/scripts/Idle.cs
+++ Unity_asia_2Dgame/Assets/scripts/Idle.cs
@@ -1,14 +1,25 @@
 ﻿
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Idle : MonoBehaviour
 {
     public GameObject final;
+    public Text textcount;
+    public int count;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        print("碰到傳送門囉");
-        final.SetActive(true);
+        if (collision.name == "傳送門")
+        {
+            final.SetActive(true);
+        }
+        if (collision.tag == "gem")
+        {
+            Destroy(collision.gameObject);
+            count++;
+            textcount.text = "Gem:" + count;
+        }
     }
 
 
